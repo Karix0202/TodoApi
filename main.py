7460b9449b7ab00d6547e9479b2badd1c723987f
Karix0202 <kari.klucz@gmail.com>
2018-07-11 22:04:31 +0200

    Register user

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from database import config
+from werkzeug.security import generate_password_hash
+import uuid
 
 app = Flask(__name__)
 
@@ -20,6 +22,21 @@ class User(db.Model):
 @app.route('/', methods = ['GET'])
 def index():
     return jsonify({'message': 'Welcome to the REST Api'})
+
+@app.route('/register', methods = ['POST'])
+def register():
+    data = request.get_json()
+
+    if User.query.filter_by(email=data['email']).first() is not None:
+        return jsonify({'code': 1})
+
+    hashed_pass = generate_password_hash(data['password'], method='sha256')
+
+    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], email=data['email'], password=hashed_pass, admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'code': 2})
 
 if __name__ == '__main__':
     app.run(debug=True)
