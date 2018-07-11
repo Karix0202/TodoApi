749cc810776321ca7ffbbaaf40b63c74a0ae380c
Karix0202 <kari.klucz@gmail.com>
2018-07-11 21:16:10 +0200

    Added User model

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from database import config
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_URI
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
 
 @app.route('/', methods = ['GET'])
 def index():
