16b4f8973ddeaaea632b106dd7ff4814b29a2ea5
Karix0202 <30869888+Karix0202@users.noreply.github.com>
2018-07-11 20:16:47 +0200

    Delete User.py

--- models/User.py
+++
@@ -1,9 +0,0 @@
-from main import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.String(50), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    name = db.Column(db.String(50))
-    password = db.Column(db.String(80))
-    name = db.Column(db.Boolean)
