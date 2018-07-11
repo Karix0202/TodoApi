1d73a509f561b0846eaccd02a872186b84f55cfb
Karix0202 <kari.klucz@gmail.com>
2018-07-11 19:32:11 +0200

    Better models

--- main.py
+++ main.py
@@ -10,24 +10,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 
 db = SQLAlchemy(app)
 
+#Index
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     return jsonify({'message': 'Welcome to the REST Api'})
 
+#User
+@app.route('/register', methods = ['POST'])
+def register():
+    request_json = request.get_json()
+
+    return jsonify(request_json)
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-#Models
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.String(50), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    name = db.Column(db.String(50))
-    password = db.Column(db.String(80))
-    name = db.Column(db.Boolean)
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(50))
-    complete = db.Column(db.Boolean)
-    user_id = db.Column(db.Integer)
---
+++ models/User.py
@@ -0,0 +1,9 @@
+from main import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(80))
+    name = db.Column(db.Boolean)
