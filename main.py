ee55894562501ffe2cdc5299dd2452ad8b70be70
Karix0202 <kari.klucz@gmail.com>
2018-07-13 15:22:15 +0200

    init

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, make_response
 from flask_sqlalchemy import SQLAlchemy
-from database import config
+from database import config as db_config
+from routes import config as routes_config
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 import jwt
@@ -9,80 +10,9 @@ from functools import wraps
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = config.SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_URI
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.String(50), unique=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(50))
-    password = db.Column(db.String(80))
-    admin = db.Column(db.Boolean)
-
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-
-        if not token:
-            return jsonify({'message' : 'Token is missing!'}), 401
-
-        try: 
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-            current_user = User.query.filter_by(public_id=data['public_id']).first()
-        except:
-            return jsonify({'message' : 'Token is invalid!'}), 401
-
-        return f(current_user, *args, **kwargs)
-
-    return decorated
-
-@app.route('/', methods = ['GET'])
+@app.route('/')
 def index():
     return jsonify({'message': 'Welcome to the REST Api'})
 
-@app.route('/register', methods = ['POST'])
-def register():
-    data = request.get_json()
-
-    if User.query.filter_by(email=data['email']).first() is not None:
-        return jsonify({'code': 1})
-
-    hashed_pass = generate_password_hash(data['password'], method='sha256')
-
-    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], email=data['email'], password=hashed_pass, admin=False)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({'code': 2})
-
-@app.route('/login')
-def login():
-    auth = request.authorization
-
-    # request.auth is email
-
-    if not auth or not auth.username or not auth.password:
-        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
-
-    user = User.query.filter_by(email=auth.username).first()
-
-    if not user:
-        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
-
-    if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=120)}, app.config['SECRET_KEY'])
-
-        return jsonify({'token' : token.decode('UTF-8')})
-
-    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
-
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.5.14', port=3333)
+    app.run(debug=True, host=routes_config.IP, port=3333)
