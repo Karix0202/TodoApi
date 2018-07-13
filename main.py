91175621338e35cb55033672683367ec8e8b1e5c
Karix0202 <kari.klucz@gmail.com>
2018-07-13 15:26:17 +0200

    Login

--- main.py
+++ main.py
@@ -39,6 +39,27 @@ def register():
 
     return jsonify({'code': 2})
 
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    # request.auth.username is email
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(email=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=120)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
 @app.route('/')
 def index():
     return jsonify({'message': 'Welcome to the REST Api'})
